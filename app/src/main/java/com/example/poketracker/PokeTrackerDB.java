902710f0797a85a5b3a34d3727cd2340c786cce0
tony-tomass <tonynguyen984@gmail.com>
2023-10-28 17:00:48 -0500

    Working SQL Database storage and ListView population of database; still need validation checks

--- app/src/main/java/com/example/poketracker/PokeTrackerDB.java
+++ app/src/main/java/com/example/poketracker/PokeTrackerDB.java
@@ -1,10 +1,13 @@
 package com.example.poketracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 import android.content.Intent;
@@ -44,6 +47,8 @@ public class PokeTrackerDB extends AppCompatActivity {
             PokeTrackerDBProvider.COLUMN10_NAME,
     };
 
+    //TextView natnum_row_tv, name_row_tv, species_row_tv, gender_row_tv, height_row_tv, weight_row_tv, level_row_tv, stat_hp_row_tv, stat_atk_row_tv, stat_def_row_tv;
+
     View.OnClickListener back_listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
@@ -58,12 +63,26 @@ public class PokeTrackerDB extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poke_tracker_bd);
 
+        /*
+        natnum_row_tv = findViewById(R.layout.row);
+        name_row_tv = findViewById(R.id.name_row_TV);
+        species_row_tv = findViewById(R.id.species_row_TV);
+        gender_row_tv = findViewById(R.id.gender_row_TV);
+        height_row_tv = findViewById(R.id.height_row_TV);
+        weight_row_tv = findViewById(R.id.weight_row_TV);
+        level_row_tv = findViewById(R.id.level_row_TV);
+        stat_hp_row_tv = findViewById(R.id.stat_hp_row_TV);
+        stat_atk_row_tv = findViewById(R.id.stat_atk_row_TV);
+        stat_def_row_tv = findViewById(R.id.stat_def_row_TV);
+         */
+
         my_cursor = getContentResolver().query(PokeTrackerDBProvider.CONTENT_URI, col_names,
                 null, null, null);
 
         Log.i("test", "Testing to see where I went wrong");
 
         pokelist_lv = findViewById(R.id.pokelist_LV);
+        registerForContextMenu(pokelist_lv);
 
         SimpleCursorAdapter simpleCursorAdapter = new SimpleCursorAdapter(
                 getApplicationContext(),
@@ -96,4 +115,50 @@ public class PokeTrackerDB extends AppCompatActivity {
         //pokemon = new LinkedList<>();
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.db_context_menu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+
+        /*
+        String selected_clause = PokeTrackerDBProvider.COLUMN1_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN2_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN3_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN4_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN5_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN6_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN7_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN8_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN9_NAME + " = ? " + " AND " +
+                PokeTrackerDBProvider.COLUMN10_NAME + " = ? "
+                ;
+
+        String[] selected_arg = {
+                natnum_row_tv.getText().toString().trim(),
+                name_row_tv.getText().toString().trim(),
+                species_row_tv.getText().toString().trim(),
+                gender_row_tv.getText().toString().trim(),
+                height_row_tv.getText().toString().trim(),
+                weight_row_tv.getText().toString().trim(),
+                level_row_tv.getText().toString().trim(),
+                stat_hp_row_tv.getText().toString().trim(),
+                stat_atk_row_tv.getText().toString().trim(),
+                stat_def_row_tv.getText().toString().trim(),
+        };
+         */
+
+        //getContentResolver().delete(PokeTrackerDBProvider.CONTENT_URI, selected_clause, selected_arg);
+        //getContentResolver().query(PokeTrackerDBProvider.CONTENT_URI, null, null,null,null);
+        //int row_deleted = getContentResolver().delete(PokeTrackerDBProvider.CONTENT_URI, selected_clause, selected_arg);
+
+        Toast.makeText(getApplicationContext(), "Entry has been deleted", Toast.LENGTH_SHORT).show();
+        //my_cursor = null;
+        return super.onContextItemSelected(item);
+        //return getContentResolver().delete(PokeTrackerDBProvider.CONTENT_URI,
+        //        PokeTrackerDBProvider.COLUMN1_NAME + "=?", new String[]{item.toString()}) > 0;
+    }
 }
